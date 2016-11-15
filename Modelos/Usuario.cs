6483c252a23e35291abe4d28f73b1568ee30b03d
AdrianInsomnio <appxims@gmail.com>
2016-11-15 06:39:59 -0300

    Agregar archivos de proyecto.

---
+++ Modelos/Usuario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// version 1 
+//23/11/2015
+
+
+
+
+namespace Modelos
+{
+   public class Usuario
+    {
+        private int idUsuario;
+            
+        public int IdUsuario
+        {
+            get { return idUsuario; }
+            set { idUsuario = value; }
+        }
+        private string nombre;
+
+        public string NombreUsuario
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string pass;
+
+        public string PassUsuario
+        {
+            get { return pass; }
+            set { pass = value; }
+        }
+
+        public Usuario()
+        {
+        }
+
+
+/// <summary>
+/// constructor completo de Usuario( idUsuario,nombre,Contraseña)
+/// </summary>
+/// <param name="id"></param>
+/// <param name="nom"></param>
+/// <param name="pas"></param>
+
+        public Usuario(int id, string nom, string pas)
+        {
+            IdUsuario = id;NombreUsuario = nom;pass = pas;
+        }
+
+
+    }
+}
---
+++ Storage/db.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace Storage
+{
+   
+   public class db
+    {
+        private static MySqlConnection con = new MySqlConnection();
+        private static MySqlCommand ccmd;
+        public db(string cn, string cmd)
+        {
+            con.ConnectionString = cn;
+            ccmd.CommandText = cmd;
+        }
+    }
+}
